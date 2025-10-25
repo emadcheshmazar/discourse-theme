6ac67bf5956a23e22882d5a393e120cb92489f03
Emad <e.cheshmazar@dariahamrah.ir>
2025-10-25 08:15:16 +0330

    fix

--- javascripts/discourse/routes/landing.js
+++
@@ -1,9 +0,0 @@
-import DiscourseRoute from "discourse/routes/discourse";
-
-export default class LandingRoute extends DiscourseRoute {
-  model() {
-    return {
-      iframeUrl: "https://emadcheshmazar.github.io/discourse-landing-build/",
-    };
-  }
-}
